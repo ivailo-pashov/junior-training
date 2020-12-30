096e965f766c46cc14c7b338c88c27f1fbe8e660
Ivaylo Pashov <Ivaylo_Pashov@epam.com>
2020-12-30 14:31:39 +0200

    Playground examples from lecture.

--- streams/src/main/java/com/epam/junx/streams/upsa/data/EmployeesProvider.java
+++ streams/src/main/java/com/epam/junx/streams/upsa/data/EmployeesProvider.java
@@ -5,5 +5,23 @@ import java.util.List;
 
 public interface EmployeesProvider {
 
+    /**
+     * @return all employees in the company flattened in a list.
+     * <p> If employee1 is a subordinate of employee2, then both employee1 and employee2 will be returned in the list.
+     * </p>
+     * <p>
+     * The organization hierarchy is modeled via tree:
+     * <ul>
+     * <li>
+     * Cyclic dependencies for subordinates are not allowed. For example, if employee2 is subordinate of employee1, it
+     * is not allowed for employee1 to be subordinate of employee2.
+     * </li>
+     * <li>
+     * Each employee can be present only once as a subordinate. For example it is not allowed for employee3 to be 
+     * subordinate of both employee1 and employee2.
+     * </li>
+     * </ul>
+     * </p>
+     */
     List<Employee> getEmployees();
 }
